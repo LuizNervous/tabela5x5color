dd42f235e9c4165fd6e0eec3e0671df5a6b8a73f
LuizNervous <luiz.gu.neves@gmail.com>
2025-11-12 17:13:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const tabela = document.getElementById('minha-tabela');
+const tamanho = 5;
+
+for (let r = 0; r < tamanho; r++) {
+  for (let c = 0; c < tamanho; c++) {
+    const degradePBaixo = (c * tamanho) + r;
+
+    const valorPasso = 255 / (tamanho * tamanho - 1);
+    const valorDecimal = Math.round(255 - (degradePBaixo * valorPasso));
+    const valorHex = valorDecimal.toString(16).padStart(2, '0')
+
+    const cor = `#${valorHex}${valorHex}${valorHex}`
+
+    const cadacor = document.createElement("div");
+    cadacor.style.backgroundColor = cor;
+    cadacor.dataset.valorDecimal = valorDecimal;
+    if (valorDecimal < 128) {
+      cadacor.style.color = "rgba(255, 255, 255, 0)";
+    }
+    else {
+      cadacor.style.color = "rgba(0,0,0,0)";
+    }
+    cadacor.dataset.colorCode = cor;
+
+    cadacor.addEventListener("click", function () {
+      const corparacopiar = this.dataset.colorCode
+      
+        this.textContent = this.dataset.colorCode;
+        this.style.color = (valorDecimal < 128) ? "#FFFFFF" : "#000000"
+     
+      navigator.clipboard.writeText(corparacopiar);
+      this.textContent = "COPIADO!";
+      setTimeout(() => {
+        this.textContent = corparacopiar
+      }, 600);
+    });
+    tabela.appendChild(cadacor);
+  }
+};
+const botao = document.querySelector("#bobo");
+botao.addEventListener("click", () => {
+  window.location.reload();
+})
+
+
+const displayBox = document.getElementById('cor-display');
+const displayCodigo = document.getElementById('cor-codigo');
+
+
